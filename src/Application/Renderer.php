1537b6973cb08e3c48f4c06e026241e64fd2148d
AslanAV <autlevaslan@yandex.ru>
2022-11-02 22:43:58 +0300

    fix Renderer.php

--- src/Application/Renderer.php
+++ src/Application/Renderer.php
@@ -4,6 +4,6 @@ namespace App\Application\Renderer;
 
 function render($filepath, $params = [])
 {
-    $templatePath = 'src' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $filepath . '.phtml';
+    $templatePath = __DIR__. '/../' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $filepath . '.phtml';
     return \App\Template\render($templatePath, $params);
 }
